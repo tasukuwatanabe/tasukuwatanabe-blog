0bbe460ea0c84e0354d0b931253d45e4c97ea2a3
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-03-16 21:59:19 +0900

    SNSリンクの遷移先を入力

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -13,13 +13,13 @@
     <li>
       <p class="mb-4 font-semibold">情報発信</p>
       <ul class="grid gap-3 pl-4 border-l-2">
-        <li><NuxtLink to="#" target="_blank">Twitter</NuxtLink></li>
+        <li><NuxtLink to="https://twitter.com/tasuku_web" target="_blank">Twitter</NuxtLink></li>
         <li>
           <NuxtLink to="https://github.com/tasukuwatanabe" target="_blank"
             >Github</NuxtLink
           >
         </li>
-        <li><NuxtLink to="#" target="_blank">Note</NuxtLink></li>
+        <li><NuxtLink to="https://note.com/tasuku_web/" target="_blank">Note</NuxtLink></li>
       </ul>
     </li>
   </ul>
