084b407c0aa8b57927cdf57aa5c353f5f1b4af4a
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-03-29 12:43:54 +0900

    Metaに関わる処理をapp.vueに移して共通化

--- app.vue
+++ app.vue
@@ -14,3 +14,35 @@ body {
   @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
 }
 </style>
+
+<script setup>
+import { withoutTrailingSlash } from 'ufo';
+
+const fetchPost = async (path) => {
+  const { data: post } = await useAsyncData(() =>
+    queryContent(path).findOne()
+  );
+
+  return post;
+}
+
+const route = useRoute();
+const post = await fetchPost(route.path);
+
+if (post) {
+  const pathWithoutSlash = withoutTrailingSlash(route.path);
+  const formattedPath = pathWithoutSlash === '/' ? '' : pathWithoutSlash;
+  const runtimeConfig = useRuntimeConfig();
+  const siteUrlWithoutSlash = withoutTrailingSlash(runtimeConfig.public.siteUrl);
+
+  useSeoMeta({
+    canonical: `${siteUrlWithoutSlash}${formattedPath}`,
+    ogDescription: post.value.description,
+    ogUrl: `${siteUrlWithoutSlash}${formattedPath}`,
+    ogImage: `${siteUrlWithoutSlash}${post.value.thumbnail}`,
+    twitterTitle: post.value.title,
+    twitterDescription: post.value.description,
+    twitterImage: `${siteUrlWithoutSlash}${post.value.thumbnail}`,
+  });
+}
+</script>
--- pages/category/[...slug].vue
+++ pages/category/[...slug].vue
@@ -11,35 +11,3 @@
     </template>
   </ContentDoc>
 </template>
-
-<script setup>
-import { withoutTrailingSlash } from 'ufo';
-
-const fetchPost = async (path) => {
-  const { data: post } = await useAsyncData(() =>
-    queryContent(path).findOne()
-  );
-
-  return post;
-}
-
-const route = useRoute();
-const post = await fetchPost(route.path);
-
-if (post) {
-  const pathWithoutSlash = withoutTrailingSlash(route.path);
-  const formattedPath = pathWithoutSlash === '/' ? '' : pathWithoutSlash;
-  const runtimeConfig = useRuntimeConfig();
-  const siteUrlWithoutSlash = withoutTrailingSlash(runtimeConfig.public.siteUrl);
-
-  useSeoMeta({
-    canonical: `${siteUrlWithoutSlash}${formattedPath}`,
-    ogDescription: post.value.description,
-    ogUrl: `${siteUrlWithoutSlash}${formattedPath}`,
-    ogImage: `${siteUrlWithoutSlash}${post.value.thumbnail}`,
-    twitterTitle: post.value.title,
-    twitterDescription: post.value.description,
-    twitterImage: `${siteUrlWithoutSlash}${post.value.thumbnail}`,
-  });
-}
-</script>
